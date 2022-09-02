3bc05a8b702a9165ade5b5cb736db5efbb8e8f67
stshsn <shooooting.star3104@gmail.com>
2022-09-02 14:09:20 +0900

    型アノテーション追加

--- gasoline_prices/main.py
+++ gasoline_prices/main.py
@@ -8,6 +8,9 @@ scraper = Scraper()
 
 @app.get("/")
 def read_root():
+    # ret = scraper.check_update(
+    #    "https://www.enecho.meti.go.jp/statistics/petroleum_and_lpgas/pl007/results.html", "2022-08-01"
+    # )
     ret = scraper.get_newest_data(
         "https://www.enecho.meti.go.jp/statistics/petroleum_and_lpgas/pl007/results.html", "2022-08-01"
     )
--- gasoline_prices/scraper.py
+++ gasoline_prices/scraper.py
@@ -1,37 +1,39 @@
 from datetime import datetime
 
-import requests
 from bs4 import BeautifulSoup as bs
+from requests import get as requests_get
+from requests import head as requests_head
+from requests.models import Response
 
 
 class Scraper:
-    def check_update(self, url, current_data):
-        res = requests.head(url)
-        last_modified = datetime.strptime(res.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z")
+    def check_update(self, url: str, current_data: str) -> bool:
+        res: Response = requests_head(url)
+        last_modified: datetime = datetime.strptime(res.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z")
 
-        current_data = datetime.fromisoformat(current_data)
-        if last_modified > current_data:
+        current_datetime: datetime = datetime.fromisoformat(current_data)
+        if last_modified > current_datetime:
             return True
         else:
             return False
 
-    def get_newest_data(self, url, current_data):
-        current_data = datetime.fromisoformat(current_data)
+    def get_newest_data(self, url: str, current_data: str) -> tuple:
+        current_datetime: datetime = datetime.fromisoformat(current_data)
 
-        with requests.get(url, stream=True) as res:
-            last_modified = datetime.strptime(res.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z")
+        with requests_get(url, stream=True) as res:
+            last_modified: datetime = datetime.strptime(res.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z")
 
-            if last_modified > current_data:
-                excel_filename = self.__get_excel_filename(res.content)
+            if last_modified > current_datetime:
+                excel_filename: str = self.__get_excel_filename(res.content)
                 return (True, excel_filename)
             else:
                 return (False, "")
 
-    def __get_excel_filename(self, raw_html):
+    def __get_excel_filename(self, raw_html: bytes) -> str:
         raw_data = bs(raw_html, "lxml")
         main_div = raw_data.find(id="main")
         result_dl = main_div.find("dl", class_="dlist_n")
         result_detail = result_dl.find_all("dd")[0]
         result_excel_anchor = result_detail.find("a", class_="excel")
-        excel_filename = result_excel_anchor["href"]
+        excel_filename: str = result_excel_anchor["href"]
         return excel_filename
