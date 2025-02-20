fdaadc3bcad29ddb35a2f5e4a393567c3f9728c7
stshsn <shooooting.star3104@gmail.com>
2025-02-20 14:21:59 +0900

    ファイルフォーマットの変更に対応(xls --> xlsx)
    - xlrd --> openpyxl
    - python 3.11 --> 3.12

--- gasoline_prices/parser.py
+++ gasoline_prices/parser.py
@@ -3,13 +3,13 @@ from io import BytesIO
 from typing import BinaryIO
 
 from requests import get as requests_get
-from xlrd import open_workbook, xldate_as_tuple
+from openpyxl import load_workbook
 
-HI_OCTANE_ROWS: range = range(9, 15)
-REGULAR_ROWS: range = range(15, 21)
-DATE_COL: int = 3  # "D"
-PRICE_COL: int = 14  # "O"
-REF_COL: int = 15  # "P"
+HI_OCTANE_ROWS: range = range(10, 16)
+REGULAR_ROWS: range = range(16, 22)
+DATE_COL: int = 4  # "D"
+PRICE_COL: int = 15  # "O"
+REF_COL: int = 16  # "P"
 
 
 class Parser:
@@ -35,14 +35,14 @@ class Parser:
 
     def __create_prices_by_type(self, oil_type, target_rows) -> list:
         self.excel_data.seek(0)
-        wb = open_workbook(file_contents=self.excel_data.read())
-        ws = wb.sheet_by_index(0)
+        wb = load_workbook(self.excel_data)
+        ws = wb.worksheets[0]
 
         prices_by_type: list = list()
         for row in target_rows:
-            survey_date = datetime(*xldate_as_tuple(ws.cell_value(rowx=row, colx=DATE_COL), wb.datemode)).date()
-            price = ws.cell_value(rowx=row, colx=PRICE_COL)
-            ref_price = ws.cell_value(rowx=row, colx=REF_COL)
+            survey_date = ws.cell(row=row, column=DATE_COL).value.date()
+            price = ws.cell(row=row, column=PRICE_COL).value
+            ref_price = ws.cell(row=row, column=REF_COL).value
             prices_by_type.append({"survey_date": survey_date, "price": price, "ref_price": ref_price})
 
         return prices_by_type
