0638ae62996cb97153cb352902c501e292db605e
stshsn <shooooting.star3104@gmail.com>
2025-08-27 15:35:27 +0900

    Excelファイル名の取得をbase_urlとhrefから合成する方法に変更

--- gasoline_prices/scraper.py
+++ gasoline_prices/scraper.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup as bs
 from requests import get as requests_get
 from requests import head as requests_head
 from requests.models import Response
+from urllib.parse import urljoin
 
 
 class Scraper:
     def __init__(self, base_url: str) -> None:
         self.base_url: str = base_url
-        self.excel_filename: str = ""
+        self.excel_href: str = ""
 
     def check_update(self, url: str, current_data: str) -> bool:
         res: Response = requests_head(self.base_url + url)
@@ -30,21 +31,22 @@ class Scraper:
                 last_modified = last_modified.replace(tzinfo=timezone.utc)
             print(f"LAST:{last_modified}, CURRENT:{current_datetime}")
             if last_modified > current_datetime:
-                excel_filename: str = self.__get_excel_filename(res.content)
-                self.excel_filename = excel_filename
+                excel_href: str = self.__get_excel_href(res.content)
+                self.excel_href = excel_href
                 return True
             else:
                 return False
 
     def get_excel_url(self) -> str:
-        return self.base_url + self.excel_filename
+        excel_url: str = urljoin(self.base_url, self.excel_href)
+        print(f"Excel URL: {excel_url}")
+        return excel_url
 
-    def __get_excel_filename(self, raw_html: bytes) -> str:
+    def __get_excel_href(self, raw_html: bytes) -> str:
         raw_data = bs(raw_html, "lxml")
         main_div = raw_data.find(id="main")
         result_ul = main_div.find("ul", class_="ulist2")
         result_detail = result_ul.find_all("li")[1]
         result_excel_anchor = result_detail.find("a")
-        excel_href_parts: list = result_excel_anchor["href"].split('/')[-1:]
-        excel_filename: str = '/'.join(excel_href_parts)
-        return excel_filename
+        excel_href: str = result_excel_anchor["href"]
+        return excel_href
